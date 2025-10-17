26df2424eaad4a2fc03aea1dd64c9deacc847eb3
AMS-JR <asj.sarjo@gmail.com>
2025-10-17 16:35:23 -0500

    init commit factory method vehicle

--- factory-method-pattern/src/main/java/com/eca/vehicle/Demo.java
+++ factory-method-pattern/src/main/java/com/eca/vehicle/Demo.java
@@ -5,7 +5,7 @@ import com.eca.vehicle.impl.TwoWheelerFactory;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class TestVehicle {
+public class Demo {
     public static void main(String[] args) {
         VehicleFactory twoWheelerFactory = new TwoWheelerFactory();
         Vehicle twoWheeler = twoWheelerFactory.createVehicle();
